111a1d624bf01d341042560e898fe6248590688b
rons003 <ronelle003@gmail.com>
2024-10-17 14:11:08 +0800

    update interface tracker system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import TileLayer from 'ol/layer/Tile';
 import Vector from 'ol/source/Vector';
 import Feature from 'ol/Feature';
 import { Point } from 'ol/geom';
-import { NgbTypeaheadModule, NgbCollapseModule, NgbModal, ModalDismissReasons, NgbCarouselModule, NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTypeaheadModule, NgbCollapseModule, NgbModal, ModalDismissReasons, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { debounceTime, distinctUntilChanged, map, Observable, OperatorFunction } from 'rxjs';
 import { RouterLink } from '@angular/router';
 
@@ -20,13 +20,13 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbTypeaheadModule, NgbCollapseModule, RouterLink, NgbCarouselModule, NgbAccordionModule],
+  imports: [RouterOutlet, NgbTypeaheadModule, NgbCollapseModule, RouterLink, NgbDropdownModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   @ViewChild('canvas', { static: true }) canvas!: ElementRef;
-  
+
   private modalService = inject(NgbModal);
   closeResult = '';
   map: Map | undefined;
@@ -40,56 +40,38 @@ export class AppComponent implements OnInit {
   items = ['Ronelle Siazon', 'Roger Tan'];
 
   ngOnInit(): void {
-    // const background = new Image();
-    // background.src = "assets/bbd52733-45f6-46e0-9e1d-02fbf9e7eb36.jpg";
-    // background.onload = () => {
-    //   const canvas: HTMLCanvasElement = this.canvas.nativeElement;
-    //   const context = canvas.getContext('2d');
-    //   if (context) {
-    //     context.drawImage(background, 0, 0);
-    //   }
-    // }
+    const background = new Image();
+    background.src = "assets/map.png";
+    background.onload = () => {
+      const canvas: HTMLCanvasElement = this.canvas.nativeElement;
+      const context = canvas.getContext('2d');
+      if (context) {
+        context.drawImage(background, canvas.width / 2 - background.width / 2,
+          canvas.height / 2 - background.height / 2);
+      }
+    }
 
-    this.map = new Map({
-      target: 'map',
-      layers: [
-        new TileLayer({
-          source: new OSM()
-        }),
-      ],
-      view: new View({
-        center: ol.fromLonLat([120.9909, 14.6633]),
-        zoom: 19,
-        minZoom: 16
-      })
-    });
+    // this.map = new Map({
+    //   target: 'map',
+    //   layers: [
+    //     new TileLayer({
+    //       source: new OSM()
+    //     }),
+    //   ],
+    //   view: new View({
+    //     center: ol.fromLonLat([120.9909, 14.6633]),
+    //     zoom: 19,
+    //     minZoom: 16
+    //   })
+    // });
 
-    this.map.on("click", this.mapClick);
-
-    const iconFeature = new Feature({
-      geometry: new Point(ol.fromLonLat([120.9909, 14.6633])),
-      name: 'Somewhere near Nottingham',
-    });
-    const marker = new VectorLayer({
-      source: new Vector({
-        features: [iconFeature]
-      }),
-      style: new Style({
-        image: new Icon({
-          anchor: [0.5, 46],
-          anchorXUnits: 'fraction',
-          anchorYUnits: 'pixels',
-          src: 'https://openlayers.org/en/latest/examples/data/icon.png'
-        })
-      })
-    })
-    this.map?.addLayer(marker)
+    // this.map.on("click", this.mapClick);
 
   }
 
-  mapClick(event: any) {
-    
-  }
+  // mapClick(event: any) {
+
+  // }
 
   search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
     text$.pipe(
